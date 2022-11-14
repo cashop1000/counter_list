7b1532ca3b6093a85655746ad7786d82be9f403e
cashop1000 <118056730+cashop1000@users.noreply.github.com>
2022-11-14 20:45:16 +0700

    Update counter_list.py

--- counter_list.py
+++ counter_list.py
@@ -1,10 +1,17 @@
 #coding:utf8
 
 
-def counter_list():
-  data = {}
-  return data
-
+def counter_list(db,result):
+    dic = {}
+    d = []
+    for x in db:
+      no = result.count(x)
+      d.append(str((x,no)))
+      k = "N%s" % no
+      if not dic.has_key(k):
+        dic[k]=[]
+      dic[k].append(x)
+    return dic
 
 if __name__ == "__main__":
-  print(counter_list())
+    print(counter_list())
